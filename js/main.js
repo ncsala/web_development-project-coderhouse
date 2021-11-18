5e8ddd6b37801db7cd424b798e4546011b16530d
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-11-18 01:34:29 -0300

    Keep adding Bem

--- js/main.js
+++ js/main.js
@@ -11,17 +11,17 @@ hamburger.addEventListener("click", function()
 //----------------------------------------------------------- /
 
 //--------------------Para Desplegar enlaces de menu hamburguesa-------------------///
-let boton = document.getElementById("header__navPrincipal__iconoHamburguesa");
-let enlaces = document.getElementById("header__navPrincipal__enlaces");
+let boton = document.getElementById("header__nav__iconoHamb");
+let enlaces = document.getElementById("header__nav__enlaces");
 let contador = 0;
 
 boton.addEventListener("click",function(){
     if(contador == 0){
-        enlaces.className = ('header__navPrincipal__enlaces circuloEnlace'); //despliega los enlaces
+        enlaces.className = ('header__nav__enlaces circuloEnlace'); //despliega los enlaces
         contador=1;// cuando esta en uno esta desplegados los enlaces
     }else{
         enlaces.classList.remove('circuloEnlace');
-        enlaces.className = ('header__navPrincipal__enlaces ocultarEnlaces');//repliega los enlaces
+        enlaces.className = ('header__nav__enlaces ocultarEnlaces');//repliega los enlaces
         contador = 0;//cuando esta en cero estan ocultos los enlaces    
     }
 })
@@ -30,7 +30,7 @@ window.addEventListener('resize', function(){
     if(screen.width > 768){
         contador=0;
         enlaces.classList.remove('circuloEnlace');
-        enlaces.className = ('header__navPrincipal__enlaces ocultarEnlaces');
+        enlaces.className = ('header__nav__enlaces ocultarEnlaces');
         hamburger.classList.remove('is-active');
 
     }
