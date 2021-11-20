dc53e07a403adc6366da9393ffeb38517da17edb
Nicolás Cáceres Sala <ncsala@gmail.com>
2021-11-20 03:19:54 -0300

    The slider is added to index

--- js/main.js
+++ js/main.js
@@ -15,14 +15,16 @@ let boton = document.getElementById("header__nav__iconoHamb");
 let enlaces = document.getElementById("header__nav__enlaces");
 let contador = 0;
 
+
 boton.addEventListener("click",function(){
     if(contador == 0){
-        enlaces.className = ('header__nav__enlaces circuloEnlace'); //despliega los enlaces
-        contador=1;// cuando esta en uno esta desplegados los enlaces
+        enlaces.classList.remove('ocultarEnlaces');
+        enlaces.className = ('header__nav__enlaces circuloEnlace'); //despliega los enlaces con una transicion en circulo
+        contador=1;// cuando esta en uno esta desplegados los enlaces del menu hamburguesa
     }else{
         enlaces.classList.remove('circuloEnlace');
         enlaces.className = ('header__nav__enlaces ocultarEnlaces');//repliega los enlaces
-        contador = 0;//cuando esta en cero estan ocultos los enlaces    
+        contador = 0;//cuando esta en cero estan ocultos los enlaces del menu hamburguesa 
     }
 })
 
@@ -32,11 +34,9 @@ window.addEventListener('resize', function(){
         enlaces.classList.remove('circuloEnlace');
         enlaces.className = ('header__nav__enlaces ocultarEnlaces');
         hamburger.classList.remove('is-active');
-
     }
 })
 //-------------------------------------------------------///
-
 
 //-------------Typewriter------------------/ 
 let app = document.getElementById('sectionUbicacion__typewriter');
